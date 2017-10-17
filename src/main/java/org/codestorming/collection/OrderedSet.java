9b8ac6aade776fe12d24b34460de368c07e90739
Thaedrik <thaedrik@codestorming.org>
2017-10-17 19:32:51 +0200

    Implemented Java8 default methods on `OrderedSet`

--- src/main/java/org/codestorming/collection/OrderedSet.java
+++ src/main/java/org/codestorming/collection/OrderedSet.java
@@ -11,8 +11,15 @@
  */
 package org.codestorming.collection;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 /**
  * An {@code OrderedSet} is the union between a {@link List} and a {@link Set}. All elements contained in the {@code
@@ -24,4 +31,62 @@ import java.util.Set;
  * @author Thaedrik [thaedrik@codestorming.org]
  * @see Collections3
  */
-public interface OrderedSet<E> extends List<E>, Set<E> {}
+public interface OrderedSet<E> extends List<E>, Set<E> {
+
+	@Override
+	default void replaceAll(UnaryOperator<E> operator) {
+		Objects.requireNonNull(operator);
+		final ListIterator<E> iter = listIterator();
+		while (iter.hasNext()) {
+			iter.set(operator.apply(iter.next()));
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	default void sort(Comparator<? super E> c) {
+		Object[] a = this.toArray();
+		Arrays.sort(a, (Comparator) c);
+		ListIterator<E> i = this.listIterator();
+		for (Object e : a) {
+			i.next();
+			i.set((E) e);
+		}
+	}
+
+	@Override
+	default Spliterator<E> spliterator() {
+		return new Spliterator<E>() {
+			private final int CHARACTERISTICS = DISTINCT | ORDERED | SIZED;
+
+			private final ListIterator<E> iterator = listIterator();
+
+			private int index = 0;
+
+			@Override
+			public boolean tryAdvance(Consumer<? super E> action) {
+				final boolean advanced = iterator.hasNext();
+				if (advanced) {
+					action.accept(iterator.next());
+					index++;
+				}
+				return advanced;
+			}
+
+			@Override
+			public Spliterator<E> trySplit() {
+				return null;
+			}
+
+			@Override
+			public long estimateSize() {
+				return size() - index;
+			}
+
+			@Override
+			public int characteristics() {
+				return CHARACTERISTICS;
+			}
+		};
+	}
+}
