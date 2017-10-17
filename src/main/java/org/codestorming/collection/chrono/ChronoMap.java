f87b2aab1ade580053e791297353ec74e9eef2b5
Thaedrik <thaedrik@codestorming.org>
2017-10-17 18:56:27 +0200

    Added javadoc on ChronoMap interface

--- src/main/java/org/codestorming/collection/chrono/ChronoMap.java
+++ src/main/java/org/codestorming/collection/chrono/ChronoMap.java
@@ -14,15 +14,37 @@ package org.codestorming.collection.chrono;
 import java.util.Map;
 
 /**
- * XXX Write comment
+ * A {@code ChronoMap} keeps the changes applied to it.
+ * <p>
+ * When new elements are added or removed to/from this map, they are also inserted respectively into the {@code added}
+ * or {@code removed} maps.
+ * <p>
+ * The added or removed maps collections contains only the elements added or removed since the last reset of this map.
  *
  * @author Thaedrik [thaedrik@codestorming.org]
  */
 public interface ChronoMap<K, V> extends Map<K, V> {
 
+	/**
+	 * Returns the elements that were added to this map and that weren't in the original one.
+	 * <p>
+	 * That is, all elements that were added since the last reset.
+	 *
+	 * @return the elements that were added to this map and that weren't in the original one.
+	 */
 	Map<K, V> added();
 
+	/**
+	 * Returns the elements that were removed from this map and that weren't in the original one.
+	 * <p>
+	 * That is, all elements that were removed since the last reset.
+	 *
+	 * @return the elements that were removed from this map and that weren't in the original one.
+	 */
 	Map<K, V> removed();
 
+	/**
+	 * Reset the internal {@code added} and {@code removed} maps collections.
+	 */
 	void reset();
 }
